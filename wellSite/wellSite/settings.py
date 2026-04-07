ad0e0223411fe51547e4e26300048ac9b5b21582
ISrbz <159147342+ISrbz@users.noreply.github.com>
2026-04-07 13:31:20 +0300

    attemt at navbar

--- wellSite/wellSite/settings.py
+++ wellSite/wellSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d1c908il9z7+1hjfbzy+8t!f4v$mj^g#*xhs4g+6kv1wrzujk('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 0#False
+DEBUG = 1 #False
 
 ALLOWED_HOSTS = []
 
